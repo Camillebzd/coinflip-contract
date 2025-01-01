7881d4763dc5531996ea55d2c894fd0ae0220b97
Camillebzd <bcamille99@gmail.com>
2025-01-01 09:50:07 +0000

    [FIX] deployment script

--- ignition/modules/Coinflip.ts
+++ ignition/modules/Coinflip.ts
@@ -3,12 +3,12 @@
 
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const RouletteModule = buildModule("RouletteModule", (m) => {
+const CoinflipModule = buildModule("CoinflipModule", (m) => {
   const Entropy = "0x23f0e8FAeE7bbb405E7A7C3d60138FCfd43d7509";
 
-  const roulette = m.contract("Roulette", [Entropy]);
+  const coinflip = m.contract("Coinflip", [Entropy]);
 
-  return { roulette };
+  return { coinflip };
 });
 
-export default RouletteModule;
+export default CoinflipModule;
